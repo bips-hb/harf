b184b702cb7ca624db084933c57da7d9a43f0bae
fouodo <fouodo@leibniz-bips.de>
2026-01-05 19:46:26 +0100

    Add single cell data example

---
+++ R/single_cell.R
@@ -0,0 +1,12 @@
+#' Example HARF dataset
+#'
+#' A small example of single cell dataset used to demonstrate and test
+#' functions in the \code{harf} package.
+#'
+#'
+#' @source Data derived from the RNAseqDB repository maintained by Memorial Sloan
+#' Kettering Cancer Center (https://github.com/mskcc/RNAseqDB).
+#' @docType data
+#' @name single_cell
+#' @usage data(single_cell)
+"single_cell"
