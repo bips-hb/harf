c843166fa013ef807a924b0fc43620127d94c162
fouodo <fouodo@leibniz-bips.de>
2026-01-30 12:40:26 +0100

    Include Cononical covariance to improve the between region meta features

--- R/h_arf.R
+++ R/h_arf.R
@@ -10,12 +10,12 @@
 #' @param cli_lab_data A data.frame of clinical or laboratory data. For example,
 #' this may be a data.frame where rows represent cell types (for single cell data)
 #' or additional clinical patient information (e.g. disease status).
+#' @param target Optional name of the target variable in \code{cli_lab_data} for supervised
+#' dimension reduction. If NULL, unsupervised dimension reduction is performed.
 #' @param omx_onset_data Optional data.frame of conditional onset omics features.
 #' @param num_trees Number of trees to grow in each Adversarial Random Forest (ARF) model.
 #' @param min_node_size Minimum number of samples required to split an internal node in the ARF model.
-#' @param encoder Method to use for encoding. Currently,
-#' only \code{fastcor} and Random Forest Auto-Encoder (\code{RFAE}) supported. Default is "fastcor".
-#' @param correlation_method Methods to compute correlation between features. Only useful for the \code{fastcor} encoder.
+#' @param correlation_method Methods to compute correlation between features.
 #' Options include "pearson", "spearman", and "kendall". Default is "spearman".
 #' @param num_clusters Number of clusters to form. If NULL, the optimal number
 #' of clusters is found by finding the elbow method.
@@ -26,6 +26,7 @@
 #' @param chunck_size Size of feature chuncks to process at a time. Default is 10.
 #' @param oob Logical indicating whether to use out-of-bag samples for density estimation.
 #' Default is FALSE. Also see \code{forde}
+#' @param always_split_meta Logical indicating whether to always split on meta features. Default is FALSE.
 #' @param family Distribution to use for density estimation of continuous features. See \code{forde} for options.
 #' @param finite_bounds Impose finite bounds on all continuous variables?
 #' If "local", infinite bounds are set to empirical extrema within leaves.
@@ -42,6 +43,9 @@
 #' @importFrom fastPLS fastcor
 #' @importFrom rsvd rpca
 #' @importFrom stats cor dist prcomp
+#' @importFrom ranger ranger
+#' @importFrom RGCCA rgcca
+#' @importFrom mixOmics block.spls block.splsda
 #' @export
 #' @seealso [h_forge], [arf::adversarial_rf], [arf::forde]
 #' @examples
@@ -76,16 +80,18 @@
 h_arf <- function (
     omx_data,
     cli_lab_data = NULL,
+    target = NULL,
     omx_onset_data = NULL,
     num_trees = 10,
     min_node_size = 5,
-    encoder = "fastcor",
+    # encoder = "fastcor",
     correlation_method = "spearman",
     num_clusters = NULL,
     num_btwn_pcs = 2,
     num_onset_pcs = 2,
     chunck_size = 10,
     oob = FALSE,
+    always_split_meta = FALSE,
     family = "truncnorm",
     finite_bounds = "no",
     alpha = 0,
@@ -93,15 +99,18 @@ h_arf <- function (
     parallel = TRUE,
     verbose = FALSE
 ) {
-  # Stop if omx_data is not a data.frame or matrix
-  if (!is.data.frame(omx_data)) {
+  # Stop if omx_data is not a data.frame or data.table
+  if (!inherits(omx_data, "data.frame")) {
     stop("omx_data must be a data.frame.")
+  } else {
+    omx_data <- as.data.frame(omx_data)
   }
   cln_omx_data <- colnames(omx_data)
-  if (!is.null(omx_onset_data) & !is.data.frame(omx_onset_data)) {
-    stop("omx_onset_data must be a data.frame.")
-  }
   if (!is.null(omx_onset_data)) {
+    if (!is.data.frame(omx_onset_data)) {
+      stop("omx_onset_data must be a data.frame.")
+    }
+    omx_onset_data <- as.data.frame(omx_onset_data)
     if (nrow(omx_data) != nrow(omx_onset_data)) {
       stop("Number of rows in omx_data must match number of rows in omx_onset_data.")
     }
@@ -111,8 +120,12 @@ h_arf <- function (
       stop("All column names in omx_onset_data must be present in omx_data.")
     }
   }
-  if (!is.null(cli_lab_data) & !is.data.frame(cli_lab_data)) {
-    stop("cli_lab_data must be a data.frame.")
+  if (!is.null(cli_lab_data)) {
+    if(!inherits(cli_lab_data, "data.frame")) {
+      stop("cli_lab_data must be a data.frame.")
+    }
+  } else {
+    cli_lab_data <- as.data.frame(cli_lab_data)
   }
   if (!is.null(cli_lab_data)) {
     cln_cli_lab_data <- colnames(cli_lab_data)
@@ -122,9 +135,9 @@ h_arf <- function (
   if (nrow(omx_data) != nrow(cli_lab_data)) {
     stop("Number of rows in omx_data must match number of rows in cli_lab_data.")
   }
-  if (!encoder %in% c("fastcor", "RFAE")) {
-    stop("encoder must be one of 'fastcor' or 'RFAE'.")
-  }
+  # if (!encoder %in% c("fastcor", "RFAE")) {
+  #   stop("encoder must be one of 'fastcor' or 'RFAE'.")
+  # }
   if (!correlation_method %in% c("pearson", "spearman", "kendall")) {
     stop("correlation_method must be one of 'pearson', 'spearman', or 'kendall'.")
   }
@@ -135,26 +148,11 @@ h_arf <- function (
     message("Clustering features...\n")
   }
   # Encoding via fast correlation and PCA
-  if (encoder == "fastcor") {
-    if (correlation_method == "spearman") {
-      cor_matrix <- fastcor(t(as.matrix(omx_data)))
-    } else {
-      # TODO: Fix me by using a fast correlation function that supports pearson and kendall
-      cor_matrix <- cor(as.matrix(omx_data), method = correlation_method)
-    }
-  }
-  # Encoding via RFAE
-  if (encoder == "RFAE") {
-    # TODO: Encoding via RFAE; review this with high-dimensional version
-    # omx_rfae <- RFAE::encode(rf = adversarial_rf(x = omx_data,
-    #                                              num_trees = num_trees,
-    #                                              min_node_size = min_node_size,
-    #                                              verbose = FALSE),
-    #                          x = omx_data,
-    #                          k = num_btwn_pcs
-    # )
-    # projected_data <- as.data.frame(omx_rfae$Z)
-    stop("RFAE encoder not yet implemented.")
+  if (correlation_method == "spearman") {
+    cor_matrix <- fastcor(t(as.matrix(omx_data)))
+  } else {
+    # TODO: Optmize me by using a fast correlation function that supports pearson and kendall
+    cor_matrix <- cor(as.matrix(omx_data), method = correlation_method)
   }
   # PCA with encoded data
   projected_data <- fast.pca(
@@ -172,7 +170,7 @@ h_arf <- function (
   # Re-split clusters that are larger than chunck_size
   # if (!is.null(chunck_size)) {
   cluster_splitting <- TRUE
-    while (cluster_splitting) {
+  while (cluster_splitting) {
     max_cluster_size <- chunck_size
     new_cluster_id <- max(feature_clusters) + 1
     cluster_splitting <- FALSE
@@ -193,51 +191,131 @@ h_arf <- function (
       }
     }
   }
-  # Compute meta cluster features for the joint structure between cluster.
+  # Merge clusters with less than num_btwn_pcs to nearest cluster
+  for (cluster in unique(feature_clusters)) {
+    ftr_in_cluster <- which(feature_clusters == cluster)
+    if (length(ftr_in_cluster) <= num_btwn_pcs) {
+      other_clusters <- unique(feature_clusters[feature_clusters != cluster])
+      cluster_center <- colMeans(projected_data[ftr_in_cluster, , drop = FALSE])
+      dists <- sapply(
+        other_clusters,
+        function(other_cluster) {
+          ftr_in_other_cluster <- which(feature_clusters == other_cluster)
+          other_cluster_center <- colMeans(projected_data[ftr_in_other_cluster, , drop = FALSE])
+          return(sqrt(sum((cluster_center - other_cluster_center)^2)))
+        }
+      )
+      nearest_cluster <- other_clusters[which.min(dists)]
+      feature_clusters[ftr_in_cluster] <- nearest_cluster
+
+    }
+  }
+  # Prepare omix regions as list of matrices for CCA.
   meta_features <- lapply(
     unique(feature_clusters),
     function(cluster) {
-      # return(
-      #   rowSums(omx_data[ , which(feature_clusters == cluster), drop = FALSE], na.rm = TRUE)
-      # )
-      cluster_data <- omx_data[ , which(feature_clusters == cluster), drop = FALSE]
-      if (ncol(cluster_data) < 2) {
-        return(as.matrix(cluster_data))
-      }
-
-      # fastcor encoder
-      if (encoder == "fastcor") {
-        # PCA of cluster features
-        pca_meta <- rpca(t(as.matrix(cluster_data)),
-                         k = num_btwn_pcs,
-                         center = TRUE,
-                         scale = TRUE)
-        # Projection of cluster features onto pcs
-        return(crossprod(t(as.matrix(cluster_data)), as.matrix(pca_meta$x)))
-      }
-
-      # Random forest encoder
-      if (encoder == "RFAE") {
-        cluster_rfae <- do.call(
-          what = "RFAE::encode",
-          args = list(
-            rf = arf::adversarial_rf(
-              x = cluster_data,
-              num_trees = num_trees,
-              min_node_size = min_node_size,
-              verbose = FALSE
-            ),
-            x = cluster_data,
-            k = num_btwn_pcs
-          )
-        )
-        return(as.matrix(cluster_rfae$Z))
-      }
+      cluster_data <- omx_data[ , which(feature_clusters == cluster),
+                                drop = FALSE]
+      return(as.matrix(cluster_data))
     }
   )
-  meta_features <- as.data.frame(meta_features)
-  colnames(meta_features) <- sprintf("cluster_%s",
-                                     ncol(meta_features))
+  names(meta_features) <- sprintf("cluster_%s",
+                                  unique(feature_clusters))
+  # Prepare clinical region RFAE of clinical/laboratory data if provided
+  if (!is.null(cli_lab_data) & (ncol(cli_lab_data) > num_btwn_pcs)) {
+    if (isTRUE(verbose)) {
+      message("Encoding clinical/laboratory data via RFAE...\n")
+    }
+    if (is.null(target)) {
+      # Unsupervised RFAE
+      cli_lab_rfae <- do.call(
+        what = "RFAE::encode",
+        args = list(
+          rf = arf::adversarial_rf(
+            x = cli_lab_data,
+            num_trees = num_trees,
+            min_node_size = min_node_size,
+            verbose = FALSE
+          ),
+          x = cli_lab_data,
+          k = num_btwn_pcs
+        )
+      )
+    } else {
+      # Supervised RFAE
+      cli_lab_rf <- ranger::ranger(
+        data = cli_lab_data,
+        dependent.variable.name = target,
+        num_trees = num_trees,
+        min_node_size = min_node_size,
+        verbose = verbose
+      )
+      cli_lab_rfae <- do.call(
+        what = "RFAE::encode",
+        args = list(
+          rf = cli_lab_rf,
+          x = cli_lab_data,
+          k = num_btwn_pcs
+        )
+      )
+    }
+    cli_lab_features <- as.matrix(cli_lab_rfae$Z)
+    meta_features <- c(meta_features,
+                       list(cli_lab = cli_lab_features))
+  }
+  # (Un)supervised CCA on all blocks to get meta features.
+  if (is.null(target)) {
+    # Unsupervised RGCCA to capture between-cluster variability
+    Z <- RGCCA::rgcca(
+      blocks = meta_features,
+      ncomp = num_btwn_pcs,
+      matrix(1, length(meta_features),
+             length(meta_features)) - diag(length(meta_features)),
+      scheme = "centroid"
+    )
+    meta_features <- as.data.frame(do.call(cbind, Z$Y))
+  } else {
+    if (is.null(cli_lab_data) | !(target %in% colnames(cli_lab_data))) {
+      stop("target must be a column name in cli_lab_data.")
+    }
+    if (is.numeric(cli_lab_data[[target]])) {
+      target_type <- "numeric"
+    } else {
+      target_type <- "categorical"
+    }
+    # Supervised CCA depending on target type
+    if (target_type == "categorical") {
+      splsda_fit <- mixOmics::block.splsda(
+        X = meta_features,
+        Y = cli_lab_data[[target]],
+        ncomp = num_btwn_pcs,
+        keepX = lapply(meta_features,
+                       function(x) {
+                         rep(ncol(x), num_btwn_pcs)
+                       })
+      )
+      meta_features <- as.data.frame(do.call(cbind, splsda_fit$variates))
+    } else {
+      spls_fit <- mixOmics::block.spls(
+        X = meta_features,
+        Y = cli_lab_data[[target]],
+        ncomp = num_btwn_pcs,
+        keepX = lapply(meta_features,
+                       function(x) {
+                         rep(ncol(x), num_btwn_pcs)
+                       }),
+        keepY = rep(ncol(cli_lab_data[[target]]), num_btwn_pcs)
+      )
+      meta_features <- as.data.frame(do.call(cbind, spls_fit$variates))
+    }
+  }
+  # Rename meta features as cluster_i.1 and cluster_i.2 etc.
+  colnames(meta_features) <- sprintf("cluster_%s.%s",
+                                     rep(sort(unique(feature_clusters)),
+                                         each = num_btwn_pcs),
+                                     rep(1:num_btwn_pcs,
+                                         times = length(unique(feature_clusters))))
+  row.names(meta_features) <- row.names(omx_data)
   # Dimension reduction of meta features if num_btwn_pcs < ncol(meta_features)
   if (num_btwn_pcs < ncol(meta_features)) {
     pca_meta <- rpca(as.matrix(t(meta_features)),
@@ -276,12 +354,12 @@ h_arf <- function (
   if (!is.null(cli_lab_data)) {
     meta_features <- data.frame(meta_features, cli_lab_data)
   }
-  # Run the meta adversarial game
+  # Run the meta adversarial game on meta features
   acc <- vector(mode = "numeric", length = length(unique(feature_clusters)) + 1L)
   names(acc) <- c("meta_model",
-                      paste0("cluster_", sort(unique(feature_clusters))))
+                  paste0("cluster_", sort(unique(feature_clusters))))
   if (isTRUE(verbose)) {
-    message("Fitting meta-cluster ARF model...\n")
+    message("Fitting the meta ARF model...\n")
   }
   meta_arf <- arf::adversarial_rf(
     x = meta_features,
@@ -299,7 +377,7 @@ h_arf <- function (
     finite_bounds = finite_bounds,
     alpha = alpha,
     epsilon = epsilon,
-    parallel = TRUE # TODO: Set parallel to parallel argument later.
+    parallel = parallel
   )
 
   meta_model <- list(meta_arf = meta_arf,
@@ -323,6 +401,11 @@ h_arf <- function (
       x = ftr_data_subset,
       num_trees = num_trees,
       min_node_size = min_node_size,
+      always.split.variables = if (isTRUE(always_split_meta)) {
+        colnames(meta_features)
+      } else {
+        NULL
+      },
       verbose = verbose
     )
     # Density estimation
@@ -334,7 +417,7 @@ h_arf <- function (
       finite_bounds = finite_bounds,
       alpha = alpha,
       epsilon = epsilon,
-      parallel = TRUE # TODO: Set parallel to parallel argument later.
+      parallel = parallel
     )
     # Export the iso_arf object
     return(list(cluster_id = cluster,
@@ -360,6 +443,7 @@ h_arf <- function (
                    cluster = feature_clusters
                  ),
                  meta_features = meta_features,
+                 clin_lab_rfae = if (exists("cli_lab_rfae")) cli_lab_rfae else NULL,
                  omx_features = cln_omx_data,
                  cli_lab_features = cln_cli_lab_data,
                  accuracy = acc
--- doc/harf.R
+++ doc/harf.R
@@ -39,16 +39,19 @@ data("single_cell")
 parallel <- ifelse(Sys.getenv("NOT_CRAN") == "true", FALSE, FALSE)
 
 ## ----harf_training, include = TRUE, eval = TRUE, message=FALSE, warning = FALSE----
+my_omx_data <- single_cell[ , - which(colnames(single_cell)  == "cell_type")]
+my_cli_lab_data <- data.frame(cell_type = single_cell$cell_type)
 harf_model <- h_arf(
- omx_data = single_cell[ , - which(colnames(single_cell)  == "cell_type")],
- cli_lab_data = data.frame(cell_type = single_cell$cell_type),
+ omx_data = my_omx_data,
+ cli_lab_data = my_cli_lab_data,
+ target = "cell_type",
  parallel = parallel,
- chunck_size = 5,
+ chunck_size = 4,
  verbose = TRUE
 )
 str(harf_model,max.level = 1)
 
-## ----harf_accuracy, include = TRUE, eval = TRUE, message=FALSE, fig.width = 5, fig.height = 3----
+## ----harf_accuracy, include = TRUE, eval = TRUE, message=FALSE, fig.width = 7, fig.height = 3----
 acc_df <- data.frame(
   Region = names(harf_model$accuracy),
   Accuracy = harf_model$accuracy
@@ -73,43 +76,21 @@ synth_single_cell <- h_forge(
   verbose = FALSE
   )
 
-## ----harf_conditional_expectation, include = TRUE, eval = TRUE, message=FALSE----
-single_cell_list <- lapply(unique(single_cell$cell_type), function (ct) {
-  ct_synth <- h_forge(
-        harf_obj = harf_model,
-        n_synth = sum(single_cell$cell_type == ct),
-        evidence = data.frame(cell_type = ct),
-        verbose = FALSE,
-        parallel = parallel
-      )
-  return(ct_synth)
-})
-cond_synth_single_cell <- do.call(rbind, single_cell_list)
-
-## ----harf_correlation_matrices, include = TRUE, eval = TRUE, message = FALSE, fig.width = 7, fig.height = 6.5----
+## ----harf_correlation_matrices, include = TRUE, eval = TRUE, message = FALSE, fig.width = 3.5, fig.height = 3----
 # Re-arrange data by grouping gene by clusters
 cluster_feature <- copy(harf_model$cluster)
 setorder(cluster_feature, cluster)
 orig_clustered <- single_cell[ , c("cell_type", cluster_feature$feature)]
 synth_clustered <- as.data.frame(synth_single_cell)[ , c("cell_type", cluster_feature$feature)]
-cond_synth_clustered <- as.data.frame(cond_synth_single_cell)[ , c("cell_type", cluster_feature$feature)]
-# Find the two largest clusters
-
 plot_corr <- function(dt, title) {
   corr_matrix <- cor(dt[ , 2:51], method = "spearman")
   corrplot(corr_matrix,
            method = "circle",
            tl.col = "black",
            tl.pos = "n",
-           # tl.srt = 45,
            title = title,
            mar = c(0, 0, 1, 0))
 }
-par(mfrow = c(2,2))
-plot_corr(orig_clustered, "Original")
-plot_corr(synth_clustered, "Synthetic")
-plot_corr(cond_synth_clustered, "Conditional resampling")
-par(mfrow = c(1, 1))
 
 ## ----harf_tsne, include = TRUE, eval = TRUE, message = FALSE, fig.width = 7.1, fig.height = 3.5----
 tsne_it <- function (sc_data, perp = 30, title = "") {
@@ -140,20 +121,64 @@ orig_plot <- tsne_it(single_cell,
 synth_plot <- tsne_it(as.data.frame(synth_single_cell),
                       perp = 30,
                       title = "Synthetic")
-cond_synth_plot <- tsne_it(as.data.frame(cond_synth_single_cell),
-                           perp = 30,
-                           title = "Conditional resampling")
 legend <- cowplot::get_legend(
   orig_plot + theme(legend.position = "bottom")
 )
 orig_plot <- orig_plot + theme(legend.position = "none")
 synth_plot <- synth_plot + theme(legend.position = "none")
-cond_synth_plot <- cond_synth_plot + theme(legend.position = "none")
-all_plots <- cowplot::plot_grid(orig_plot, synth_plot, cond_synth_plot, ncol = 3)
-plot_grid(
-  all_plots,
-  legend,
-  ncol = 1,
-  rel_heights = c(1, 0.2)
+all_plots <- cowplot::plot_grid(orig_plot,
+                                synth_plot,
+                                ncol = 2)
+par(mfrow = c(1,2))
+plot_corr(orig_clustered, "Original")
+plot_corr(synth_clustered, "Synthetic")
+par(mfrow = c(1, 1))
+print(all_plots)
+
+## ----harf_tsne_plot, include = FALSE, eval = FALSE, message = FALSE, fig.width = 7, fig.height = 6.5----
+# par(mfrow = c(1, 1))
+# plot_grid(
+#   all_plots,
+#   legend,
+#   ncol = 1,
+#   rel_heights = c(1, 0.2)
+# )
+
+## ----harf_conditional_expectation, include = TRUE, eval = TRUE, message=FALSE, fig.width = 7, fig.height = 3.5----
+sub_cell_type <- c("lung", "liver", "esophagus_muc")
+single_cell_list <- lapply(sub_cell_type, function (ct) {
+  ct_synth <- h_forge(
+        harf_obj = harf_model,
+        n_synth = sum(single_cell$cell_type == ct),
+        evidence = data.frame(cell_type = ct),
+        verbose = FALSE,
+        parallel = parallel
+      )
+  return(ct_synth)
+})
+cond_synth_single_cell <- do.call(rbind, single_cell_list)
+cond_synth_clustered <- as.data.frame(cond_synth_single_cell)[ , c("cell_type", cluster_feature$feature)]
+cond_synth_plot <- tsne_it(as.data.frame(cond_synth_single_cell),
+                           perp = 30,
+                           title = "Conditional resampling")
+sub_legend <- cowplot::get_legend(
+  cond_synth_plot + theme(legend.position = "none")
 )
+cond_synth_plot <- cond_synth_plot + theme(legend.position = "none")
+sub_single_cell <- orig_clustered[orig_clustered$cell_type %in% sub_cell_type , ]
+sub_orig_plot <- tsne_it(sub_single_cell,
+                     perp = 30,
+                     title = "Original")
+legend <- cowplot::get_legend(
+  orig_plot + theme(legend.position = "bottom")
+)
+sub_all_plots <- cowplot::plot_grid(sub_orig_plot,
+                                cond_synth_plot,
+                                ncol = 2)
+par(mfrow = c(1,2))
+plot_corr(sub_single_cell, "Original")
+plot_corr(cond_synth_clustered, "Synthetic")
+par(mfrow = c(1, 1))
+plot_grid(sub_all_plots, sub_legend, ncol = 1, rel_heights = c(1, 0.2))
+par(mfrow = c(1, 1))
 
